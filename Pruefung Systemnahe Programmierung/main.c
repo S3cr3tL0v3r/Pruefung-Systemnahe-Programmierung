dd2683f44651d624299b6971a6010d665d24f3d8
Henrik Kaltenbach <henrik.kaltenbach49@gmail.com>
2021-05-16 21:36:03 +0200

    ups, main vergessen

--- Pruefung Systemnahe Programmierung/main.c
+++ Pruefung Systemnahe Programmierung/main.c
@@ -10,23 +10,23 @@
  */ 
 
 #include <avr/io.h>
-#include "include/a1_traffic_light.h"
-#include "include/a2_buttons_LED_polling.h"
-#include "include/a3_buttons_led_interrupts.h"
-#include "include/a4_summer.h"
-#include "include/a5_dim_LED.h"
+//#include "include/a1_traffic_light.h"
+//#include "include/a2_buttons_LED_polling.h"
+//#include "include/a3_buttons_led_interrupts.h"
+//#include "include/a4_summer.h"
+//#include "include/a5_dim_LED.h"
 #include "include/a6_read_analog_2chan.h"
-#include "include/a7_running_light_8bit.h"
+//#include "include/a7_running_light_8bit.h"
 
 int main(void)
 {
     /* setup function */
-	runningLightSetup();
+	readAnalog2chanSetup();
 	
     while (1) 
     {
 		/* loop function */
-		runningLightLoop();
+		readAnalog2chanLoop();
     }
 }
 
